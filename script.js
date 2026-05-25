e645978f98e13d674e7ef5f77a77cf2e0d5a82b1
isabel-afk <mariaisabelsantos450@gmail.com>
2026-05-25 16:50:21 -0500

    atualizacao de atividade

---
+++ script.js
@@ -0,0 +1,74 @@
+console.log("Boa noite hoje é sexta-feira")
+
+// console
+
+console.error("Este é um Erro")
+console.warn("Este é um aviso") 
+
+// variáveis
+
+var x = 10
+
+// variáveis modernas
+
+let y = 20
+const z = 30
+
+console.log(x)
+console.log(y)
+console.log(z)  
+
+// tipos de dados
+
+//texto
+const name = "Maria Isabel"
+console.log(name)
+console.log(typeof name)
+
+// numerico
+
+const shirtsQty = 4
+console.log(shirtsQty)
+console.log(typeof shirtsQty)
+
+// numero decimal
+
+const decimal = 20.1
+console.log(decimal)
+console.log(typeof decimal)
+
+// booleano = true or false
+
+const isApproved = false
+console.log(isApproved)
+console.log(typeof isApproved)
+
+// null
+
+let surname = null
+console.log(surname)
+console.log(typeof surname)
+
+// array
+
+let languages = ["JavaScript", "PHP", "Python"]
+console.log(languages)
+console.log(typeof languages)
+
+// object
+
+const user = {email: "mariaisabelsantos@gmail.com", password: "tente123", age: "30"}
+console.log(user)
+console.log(typeof user)
+
+// metodo de string
+
+const fullname = "Maria Isabel Santos"
+console.log(fullname.length)
+console.log(typeof fullname)
+
+//metodo string de lista
+
+const stringToArray = fullnme.split(" ")
+console.log(stringToArray)
+console.log(fullname.toLowerCase())
